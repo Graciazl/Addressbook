142a6871165258fc902406b6152436ebb7c68468
Gracia <gracia@devlevelup.com>
2016-05-29 00:32:24 -0400

    no message

---
+++ app.js
@@ -0,0 +1,168 @@
+/**
+ * Created by Gracia on 16/4/18.
+ */
+
+
+//add contact
+
+var contact = [];
+
+function newContact() {
+    var newContact = {};
+    newContact.firstname = document.getElementById('firstname').value;
+    newContact.lastname = document.getElementById('lastname').value;
+    newContact.telephone = document.getElementById('telephone').value;
+    return newContact;
+}
+
+function addContact() {
+    if (localStorage.getItem('info') !== null ) {
+        contact = JSON.parse(localStorage.getItem('info'));
+    }
+    contact.push(newContact());
+    localStorage.setItem('info',JSON.stringify(contact));
+    alert('Your information has been added.');
+}
+
+var id = document.getElementById('add');
+addEvent(id,'click',addContact);
+
+
+//discard input
+var dis = document.getElementById('discard');
+function discardContact() {
+    document.getElementById('inputTable').reset();
+}
+addEvent(dis,'click',discardContact);
+
+
+//check list
+var list = document.getElementById('listView');
+addEvent(list,'click',creatTable);
+
+
+//sort
+
+var list = JSON.parse(localStorage.getItem('info'));
+
+function sortFirstName() {
+    list.sort(function(a,b) {
+        if (a.firstname > b.firstname) {
+            return 1;
+        } else if (a.firstname < b.firstname) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    localStorage.setItem('info',JSON.stringify(list));
+    creatTable();
+}
+
+function sortLastName() {
+    list.sort(function(a,b) {
+        if (a.lastname > b.lastname) {
+            return 1;
+        } else if (a.lastname < b.lastname) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    localStorage.setItem('info',JSON.stringify(list));
+    creatTable();
+}
+
+function sortNumber() {
+    list.sort(function(a,b){
+        return a.telephone - b.telephone;
+    });
+    localStorage.setItem('info',JSON.stringify(list));
+    creatTable();
+}
+
+var sf = document.getElementById('sortFirstName');
+addEvent(sf,'click',sortFirstName);
+var sl = document.getElementById('sortLastName');
+addEvent(sl,'click',sortLastName);
+var st = document.getElementById('sortTel');
+addEvent(st,'click',sortNumber);
+
+
+//creat table
+
+function creatTable() {
+    var myTB = document.getElementById('tableData');
+    function newTable() {
+        var dataTB = JSON.parse(localStorage.getItem('info'));
+        for (var i = 0; i < dataTB.length; i++) {
+            var newRow = document.createElement('tr');
+            var obj = dataTB[i];
+            for (var x in obj) {
+                var newCell = document.createElement('td');
+                var newText = document.createTextNode(obj[x]);
+                newCell.appendChild(newText);
+                newRow.appendChild(newCell);
+            }
+            document.getElementById('tableData').appendChild(newRow);
+        }
+        highLight();
+    }
+    if (myTB.rows.length === 0) {
+        return newTable();
+    } else {
+        myTB.innerHTML = '';
+        return newTable();
+     }
+}
+
+//highlight rows
+
+var rowIdx;
+
+function highLight() {
+    var table = document.getElementById("tableData");
+    var rows = table.getElementsByTagName("tr");
+    var selectedRow;
+
+    function SelectRow(row) {
+        if (selectedRow !== undefined) {
+            selectedRow.style.background = "#fbfbfb";
+        }
+        selectedRow = row;
+        selectedRow.style.background = "#44dee0";
+    }
+
+    for (var i = 0; i < rows.length; i++) {
+        (function (idx) {
+            addEvent(rows[idx], "click", function() {
+                SelectRow(rows[idx]);
+                rowIdx = idx;
+            });
+        })(i);
+    }
+}
+
+//delete row
+
+var del = document.getElementById('del');
+addEvent(del,'click',delRow);
+
+function delRow() {
+    var table = document.getElementById("tableData");
+    table.deleteRow(rowIdx);
+    list.splice(rowIdx,1);
+    localStorage.setItem('info',JSON.stringify(list));
+}
+
+//event control
+
+function addEvent(element, evt, callback) {
+    if (element.addEventListener) {
+        element.addEventListener(evt, callback, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + evt, callback);
+    } else {
+        element["on" + evt] = callback;
+    }
+}
